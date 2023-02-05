2b4b65dadf3516966863ada388850658aeb1eb7d
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-02-05 21:07:32 +0300

    tariff screen

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,20 @@
-function testWebP(callback) {
+document.querySelectorAll('a[href^="#"').forEach(link => {
 
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
 
-testWebP(function (support) {
+      let href = this.getAttribute('href').substring(1);
 
-    if (support == true) {
-        document.querySelector('body').classList.add('webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-    }
+      const scrollTarget = document.getElementById(href);
+
+      const topOffset = 0;
+
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
 });
