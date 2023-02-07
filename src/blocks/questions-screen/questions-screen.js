05fd71ffa0fc77d3699d2d886c486433f09b3ac2
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-02-07 17:57:29 +0300

    edits

--- src/blocks/questions-screen/questions-screen.js
+++ src/blocks/questions-screen/questions-screen.js
@@ -7,6 +7,14 @@ function unwrapQuestioncard() {
 
       if (event.target.id === 'question_1_detailed') {
         questionCard = document.querySelector('#question_1').classList.toggle('question-card__toggle-button--pressed');
+      } else if (event.target.id === 'question_2_detailed') {
+        questionCard = document.querySelector('#question_2').classList.toggle('question-card__toggle-button--pressed');
+      } else if (event.target.id === 'question_3_detailed') {
+        questionCard = document.querySelector('#question_3').classList.toggle('question-card__toggle-button--pressed');
+      } else if (event.target.id === 'question_4_detailed') {
+        questionCard = document.querySelector('#question_4').classList.toggle('question-card__toggle-button--pressed');
+      } else if (event.target.id === 'question_5_detailed') {
+        questionCard = document.querySelector('#question_5').classList.toggle('question-card__toggle-button--pressed');
       }
     })
   })
